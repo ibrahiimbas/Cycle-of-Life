5b355fa6fe035f20c9d712e7bea8724a36959308
İbrahim Emre Baş <ibrahimbas1414@gmail.com>
2024-03-24 12:56:00 +0300

    script cleaning

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mime;
 using TMPro;
-using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
