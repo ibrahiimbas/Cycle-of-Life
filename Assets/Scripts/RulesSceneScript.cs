44e5907bf93d8544c71e2b903eb2c99cfb365863
İbrahim Emre Baş <ibrahimbas1414@gmail.com>
2026-03-18 12:35:05 +0300

    Show player's local time and deleting unused assets

--- Assets/Scripts/RulesSceneScript.cs
+++ Assets/Scripts/RulesSceneScript.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
+using System.Globalization;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -13,13 +14,22 @@ public class RulesSceneScript : MonoBehaviour
 {
     [SerializeField] private Button backButton;
     [SerializeField] private Button exitButton;
+    [SerializeField] private TextMeshProUGUI currentTimeText;
+    private string formattedTime;
     
     public void Start()
     {
         backButton.onClick.AddListener(JumpToSimulation);
-        exitButton.onClick.AddListener(ExitSimulation);  
+        exitButton.onClick.AddListener(ExitSimulation); 
     }
-    
+
+    private void Update()
+    {
+        DateTime currentTime = DateTime.Now;
+        formattedTime = currentTime.ToString("hh:mm tt",CultureInfo.InvariantCulture);
+        currentTimeText.text = formattedTime;
+    }
+
     private void JumpToSimulation()
     {
         SceneManager.LoadScene("CycleofLife", LoadSceneMode.Single);
