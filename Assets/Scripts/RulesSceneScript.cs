f137b82764c6959e4e72de6892071418ef9eb175
İbrahim Emre Baş <ibrahimbas1414@gmail.com>
2026-03-18 15:48:07 +0300

    Render settings and some improvements

--- Assets/Scripts/RulesSceneScript.cs
+++ Assets/Scripts/RulesSceneScript.cs
@@ -9,18 +9,33 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 using UnityEngine.UI;
 using Button = UnityEngine.UI.Button;
+using Image = UnityEngine.UI.Image;
+using Toggle = UnityEngine.UI.Toggle; 
 
 public class RulesSceneScript : MonoBehaviour
 {
     [SerializeField] private Button backButton;
     [SerializeField] private Button exitButton;
+    [SerializeField] private Button closeNotepadTabButton;
+    [SerializeField] private Toggle notepadBottomToggle;
     [SerializeField] private TextMeshProUGUI currentTimeText;
+    [SerializeField] private GameObject notepadTab;
+    [SerializeField] private Image notepadImage;
+    [SerializeField] private Sprite notepadOpenSprite;
+    [SerializeField] private Sprite notepadClosedSprite;
+    [SerializeField] private TextMeshProUGUI notepadTabHeaderText;
+    
     private string formattedTime;
+    private Color originalHeaderColor;
+    [SerializeField] private Color inactiveHeaderColor;
     
     public void Start()
     {
         backButton.onClick.AddListener(JumpToSimulation);
-        exitButton.onClick.AddListener(ExitSimulation); 
+        exitButton.onClick.AddListener(ExitSimulation);
+        closeNotepadTabButton.onClick.AddListener(CloseNotepadTab);
+        notepadBottomToggle.onValueChanged.AddListener(OnNotepadToggleChanged);
+        originalHeaderColor = notepadTabHeaderText.color;
     }
 
     private void Update()
@@ -38,5 +53,31 @@ public class RulesSceneScript : MonoBehaviour
     private void ExitSimulation()
     {
         Application.Quit();
+    }
+
+    private void CloseNotepadTab()
+    {
+        notepadTab.SetActive(false);
+        notepadBottomToggle.gameObject.SetActive(false);
+    }
+    
+    private void OnNotepadToggleChanged(bool isOn)
+    {
+        if (isOn)
+        {
+            if (notepadImage != null && notepadOpenSprite != null)
+            {
+                notepadImage.sprite = notepadOpenSprite;
+                notepadTabHeaderText.color = originalHeaderColor;
+            }
+        }
+        else
+        {
+            if (notepadImage != null && notepadClosedSprite != null)
+            {
+                notepadImage.sprite = notepadClosedSprite;
+                notepadTabHeaderText.color = inactiveHeaderColor;
+            }
+        }
     }
 }
